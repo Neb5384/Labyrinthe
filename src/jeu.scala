55bdc3d446d5c1cab335968a7c182dd527d27770
Benno Weber <bennopaul.weber@gmail.com>
2025-01-09 13:20:26 +0100

    start of labyrinth algorithm

--- src/jeu.scala
+++ src/jeu.scala
@@ -1,5 +1,7 @@
 import hevs.graphics.FunGraphics
-import java.awt.{Color,Rectangle}
+
+import java.awt.{Color, Rectangle}
+import scala.runtime.BooleanRef
 
 class Display {}
 object Display {
@@ -13,15 +15,35 @@ object Display {
   }
 }
 
+
+
 object Maze {
   def generateMaze(width: Int, height: Int): Array[Array[Int]] = {
     var maze: Array[Array[Int]] = Array.ofDim[Int](width, height)
+    var cells: Array[Array[Int]] = Array.ofDim[Int]((width/2-1),(height/2-1))
     /***
+     * _ -> unvisited
      * 1 -> wall
      * 0 -> visited
+     * 2 -> visited and backtracked
      */
-    var initialPosX = 0
-    var initialPosy = 0
+    val posX: Int = 0
+    val posY: Int = 0
+    cells(0)(0) = 0
+    var visitedAll: Boolean = false
+    do{
+      var validArray: Array[Int] = new Array[Int](4) // Array that stores which values are empty
+      //left-down-right-up
+      if ((posX -1 < 0)||)
+
+
+      visitedAll = true
+      for (x <- cells; y <- x){
+        if (y != 0) visitedAll = false
+      }
+    }
+    while (!visitedAll)
+
 
     maze
   }
