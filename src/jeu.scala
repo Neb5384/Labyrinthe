8b9406e5d34cd3f9e099c708442a7828eda4e7f2
Benno Weber <bennopaul.weber@gmail.com>
2025-01-15 11:58:11 +0100

    BIG COMMIT everything works except player

--- src/jeu.scala
+++ src/jeu.scala
@@ -20,36 +20,65 @@ object Display {
     }
   }
 }
+object Player {
+  case class State(player: (Double, Double)) {
+    def newState(dir: Int): State = {
+      val (x, y) = player
+      val (newx, newy) = dir match {
+        case 1 => (x, y - 1) // up
+        case 2 => (x, y + 1) // down
+        case 3 => (x - 1, y) // left
+        case 4 => (x + 1, y) // right
+        case _ => (x, y)
+      }
+      val newplayer: (Double, Double) = (newx, newy)
+      State (newplayer)
+    }
+  }
+}
+
+
 object Maze {
+
   def randomInValid(array : Array[Int]): Int = {
-    var numOfValid: Int = 0
+  var numOfValid: Int = 0
     for (i <- array){
       if (i == 1) numOfValid += 1
-    }
+      }
     var ran: Int = (math.random() * numOfValid).toInt + 1
     var validcount: Int = 0
-    for (i <- array.indices){
-      if(array(i) == 1) validcount += 1
-      if (validcount == ran) return i
-    }
+      for (i <- array.indices){
+        if(array(i) == 1) validcount += 1
+          if (validcount == ran) return i
+      }
     15
   }
-  def generateMaze(width: Int, height: Int): Array[Array[Int]] = {
-    var maze: Array[Array[Int]] = Array.ofDim[Int](width, height)
-    var cells: Array[Array[Int]] = Array.ofDim[Int]((width/2-1),(height/2-1))
-    /***
-     * 0 -> unvisited
-     * 1 -> wall
-     * 2 -> visited
-     * 3 -> visited and backtracked
-     */
-    var posX: Int = 0
-    var posY: Int = 0
-    cells(0)(0) = 2
-    var visitedAll: Boolean = false
-    do{
-      var validArray: Array[Int] = new Array[Int](4) // Array that stores which values are valid
-      //left-down-right-up
+def generateMaze(width: Int, height: Int): Array[Array[Int]] = {
+  var maze: Array[Array[Int]] = Array.ofDim[Int](width, height)
+
+  for ((y, yPos) <- maze.zipWithIndex;
+       (x, xPos) <- y.zipWithIndex) {
+    if((yPos%2) == 0 || (xPos%2) == 0){
+      maze(yPos)(xPos) = 1
+    }
+  }
+
+  var cells: Array[Array[Int]] = Array.ofDim[Int]((width/2),(height/2))
+
+
+  /***
+  * 0 -> unvisited
+  * 1 -> wall
+  * 2 -> visited
+  * 3 -> visited and backtracked
+  */
+  var posX: Int = 0
+  var posY: Int = 0
+  cells(0)(0) = 2
+  var visitedAll: Boolean = false
+  do{
+    var validArray: Array[Int] = new Array[Int](4) // Array that stores which values are valid
+    //left-down-right-up
       if (posX -1 < 0) {
         validArray(0) = 0
       }else if(cells(posY)(posX -1) == 2 || cells(posY)(posX -1) == 3){
@@ -68,7 +97,7 @@ object Maze {
 
       if (posX +1 >= cells.length) {
         validArray(2) = 0
-      }else if(cells(posX+1)(posY) == 2 || cells(posX+1)(posY) == 3){
+      }else if(cells(posY)(posX+1) == 2 || cells(posY)(posX+1) == 3){
         validArray(2) = 0
       }else{
         validArray(2) = 1
@@ -86,10 +115,11 @@ object Maze {
       var direction: Int = randomInValid(validArray) // 0: left 1: down 2: right 3: up
       println(direction)
       direction match{
-        case 0 => posX -= 1
-        case 1 => posY -= 1
-        case 2 => posX += 1
-        case 3 => posY += 1
+        case 0 => maze(posY*2 +1)(posX*2 -1 +1) = 0; posX -= 1
+        case 1 => maze(posY*2 -1 +1)(posX*2 +1) = 0; posY -= 1
+        case 2 => maze(posY*2 +1)(posX*2 +1 +1) = 0; posX += 1
+        case 3 => maze(posY*2+ 1 +1)(posX*2 +1) = 0;posY += 1
+        case _ =>
       }
 
       cells(posY)(posX) = 2
@@ -99,11 +129,25 @@ object Maze {
       for (x <- cells; y <- x){
         if (y != 2) visitedAll = false
       }
+    if (direction == 15){
+      var foundNewStart: Boolean = false
+      while (!foundNewStart){
+        posY = (math.random()*cells.length).toInt
+        posX = (math.random()*cells(0).length).toInt
+        if(cells(posY)(posX) == 2){
+          foundNewStart = true
+        }
+      }
     }
-    while (!visitedAll)
-    maze
   }
+  while (!visitedAll)
+  println("maze generated !")
+
+
+  maze
 }
+}
+/***
 object Player {
 
   case class State(player: (Double, Double)) {
@@ -121,29 +165,23 @@ object Player {
     }
   }
 }
-
+**/
 object JEU extends App{
-  var WIDTH: Int = 96 *Display.pixel_value
-  if (WIDTH%2==1){
+  var WIDTH: Int = 96
+
+  if (WIDTH%2==0){
     WIDTH+=1
   }
-  var HEIGHT: Int = 64 *Display.pixel_value
-  if (HEIGHT%2==1){
+  var HEIGHT: Int = 96
+  if (HEIGHT%2==0){
     HEIGHT+=1
   }
-  val gameWindow : FunGraphics = new FunGraphics(WIDTH,HEIGHT)
 
-  var maze: Array[Array[Int]] = Array.ofDim(WIDTH,HEIGHT)
+          val gameWindow : FunGraphics = new FunGraphics(WIDTH*Display.pixel_value,HEIGHT*Display.pixel_value)
 
-  maze(0)(0)=1
-  maze(8)(5)=1
-  maze(4)(3)=1
-  maze(63)(63)=1
-  for (i<- 0 until WIDTH){
-    maze(i)(32)=1
-  }
-  maze(9)(32)=0
+          var maze: Array[Array[Int]] = Maze.generateMaze(WIDTH,HEIGHT)
+
+          Display.blit(maze)
   maze(1)(1)=4
 
-  Display.blit(maze)
-}
+        }
